33f7c22a11bec8a9e7b265c339382ecf46229dc2
chanteloatman <31936830+chanteloatman@users.noreply.github.com>
2018-01-29 11:19:50 -0500

    add files

---
+++ app_html.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const fs = require('fs');//module part of the node core
+
+const hostname = '127.0.0.1';
+const port = process.env.PORT || 3000;
+const user = process.env.USER;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type' : 'text/html'});
+
+  var file = fs.createReadStream('index.html');
+  file.pipe(res);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/Logged in as ${user}`);
+});
